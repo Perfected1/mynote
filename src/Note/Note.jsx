bf92178b032a782750e0f610b9c0edfee7d8f98e
Chike Nnamadim <38839028+Perfected1@users.noreply.github.com>
2020-08-15 14:30:45 +0100

    linked frontend to firebase, User can add and remove notes

--- src/Note/Note.jsx
+++ src/Note/Note.jsx
@@ -1,20 +1,28 @@
-import React, { Component } from 'react'
-import'./Note.css';
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import './Note.css';
+import PropTypes from 'prop-types';
 
 class Note extends Component{
 
-    constructor(props) {
+    constructor(props){
         super(props);
-        this.noteContent = props.noteContent
-        this.noteId = props.noteId
+        this.noteContent = props.noteContent; 
+        this.noteId = props.noteId; 
+        this.handleRemoveNote = this.handleRemoveNote.bind(this);
     }
-    render(props) {
+
+    handleRemoveNote(id){
+        this.props.removeNote(id);
+    }
+
+    render(){
         return(
             <div className="note fade-in">
-                <div>
-                </div>
-                <p className="noteContent">{this.noteContent}</p>
+                <span className="closebtn" 
+                      onClick={() => this.handleRemoveNote(this.noteId)}>
+                      &times;
+                </span>
+                <p className="noteContent">{ this.noteContent }</p>
             </div>
         )
     }
@@ -23,4 +31,5 @@ class Note extends Component{
 Note.propTypes = {
     noteContent: PropTypes.string
 }
-export default Note 
+
+export default Note;
